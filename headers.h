bb02f45c0b34f42ffa3703521cbea541b1158251
natsukmoe <charlieyan@126.com>
2020-04-12 09:22:26 +0800

    1

---
+++ headers.h
@@ -0,0 +1,13 @@
+//
+//  headers.h
+//  molecular-weight-calculator
+//
+//  Created by 夏欣萌 on 2020/4/12.
+//  Copyright © 2020 夏欣萌. All rights reserved.
+//
+
+#ifndef headers_h
+#define headers_h
+#include <cstdio>
+using namespace std;
+#endif /* headers_h */
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  molecular-weight-calculator
+//
+//  Created by 夏欣萌 on 2020/4/12.
+//  Copyright © 2020 夏欣萌. All rights reserved.
+//
+
+#include "headers.h"
+
+int main(int argc,char** argv){
+    
+}
